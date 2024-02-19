a321f9161d537abdf1cfd5a5402bb8fefce82ce2
9-coding <9.coding.lab@gmail.com>
2024-02-19 14:01:54 +0900

    FEAT: add predict.py - fastapi communication

---
+++ predict.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
+from typing import List
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+app = FastAPI()
+
+
+class DetectionResult(BaseModel):
+    detected_objects: List[str]
+    filename: str
+
+#uvicorn predict:app --reload
+@app.post("/detect")
+async def detect(image: UploadFile = File(...)):
+    img = await image.read()
+    with open(image.filename, "wb") as f:
+        f.write(img)
+
+    model = YOLO("../best (2).pt")  # build a new model from scratch
+    results = model.predict(source=image.filename)
+    names = model.names
+
+    detected_objects = []
+    for r in results:
+        for c in r.boxes.cls:
+            print(names[int(c)])
+            detected_objects.append(names[int(c)])
+
+    return {"detected_objects": detected_objects, "filename": image.filename}
+
